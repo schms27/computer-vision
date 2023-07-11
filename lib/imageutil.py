88d2b516e938ee81a1838d11d34c32fe77b52abd
Schmid Simon <schms27@gmail.com>
2023-07-11 15:04:30 +0200

    add hsv-rgb stuff and merge with grayscale

--- lib/imageutil.py
+++ lib/imageutil.py
@@ -2,10 +2,10 @@ import cv2
 from matplotlib import pyplot as plt
 
 
-def show_image(image = None, title = "Image", size = 5):
+def show_image(image = None, title = "Image", size = 5, color_conversion = cv2.COLOR_BGR2RGB):
     width, height = image.shape[0], image.shape[1]
     aspect_ratio = width/height
     plt.figure(figsize=(size * aspect_ratio,size))
-    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.imshow(cv2.cvtColor(image, color_conversion))
     plt.title(title)
     plt.show()
